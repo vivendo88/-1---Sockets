6af9334c944f5cae86a3d0deed94806d72d65052
vivendo88 <107193245+vivendo88@users.noreply.github.com>
2026-09-10 13:36:17 -0300

    Add files via upload

---
+++ Cliente_Arcondicionado.py
@@ -0,0 +1,51 @@
+#####################################################
+#                                                   #
+# Título do trabalho: Trabalho de Sockets           #
+# Disciplina: Redes de Computadores PPComp          #
+#                                                   #
+#####################################################
+
+from Config import *
+from Message import *
+from ClientUtil import *
+import socket
+
+deviceID = None
+
+####################
+# Inicializando... #
+####################
+if __name__ == '__main__':
+	print('Inicializando cliente: Ar-Condicionado...')
+	try:
+		connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		destination = (SERVIDOR, PORTA)
+		connection.connect(destination)
+	except:
+		print(f'Falha ao tentar se conectar com o servidor {SERVIDOR} porta {PORTA}')
+		exit()
+
+	# Registra no servidor como atuador suportado
+	device = Device(connection, NUM_LAMPADA)
+	roomDict = ClientRegister(device)
+	if roomDict != None:
+		deviceID, roomID, roomName = SelectRoom(device, roomDict)
+		if deviceID != None:
+			while True:
+				print(f'\n==> Ambiente [{roomID}] {roomName}')
+				msg = ReceiveMessage(connection, device)
+				if msg.code == MSG_LAMPADA:
+					print('Acionamento recebido do servidor!!!')
+					print('#####################################')
+					if msg.action == LUZ_ACESA:
+						print('       AR-CONDICIONADO LIGADO')
+					elif msg.action == LUZ_APAGADA:
+						print('      AR-CONDICIONADO DESLIGADO')
+					else:
+						print(f'Ação inválida: {msg.action}')
+					print('#####################################')
+					msg = MessageStatus()
+					connection.send(msg.pack(deviceID, ACAO_EXECUTADA))
+				else:
+					print('Mensagem inválida code:', msg.code)
+		connection.close()
